e8743ada690acb471a8c41afa2544ac49b753654
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2025-01-28 08:27:26 -0300

    Video Player added

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,17 @@
+"use client"
+
+import Video from 'next-video';
+import busVideo from '/videos/video.mp4';
+
 export default function Home() {
   return (
-    <div className="">
-   
+    <div className="bg-background w-full h-screen text-center flex flex-col items-center gap-16 pt-12">
+		<h1 className='text-white text-4xl font-semibold'>
+			Welcome to AI Object Detection
+		</h1>
+		<div className='w-[500px] h-[280px]'>
+			<Video src={busVideo} width={500} height={280} />
+		</div>
     </div>
   );
 }
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -9,8 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#1E1E1E"
       },
     },
   },
